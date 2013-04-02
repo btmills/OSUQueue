4e61a6eae15187efbc6062694c3638640dca6e4a
Brandon Mills <mills.brandont@gmail.com>
2013-04-02 16:42:41 -0400

    Added unique value restriction like sets

--- src/net/bmills/osuqueue/Queue.java
+++ src/net/bmills/osuqueue/Queue.java
@@ -1,5 +1,7 @@
 package net.bmills.osuqueue;
 
+import java.util.HashMap;
+
 public class Queue<E> {
 	
 	private class Entry<T> {
@@ -18,39 +20,54 @@ public class Queue<E> {
 	private int size;
 	private Entry<E> head;
 	private Entry<E> tail;
+	private HashMap<E, Entry<E>> map;
 	
 	Queue() {
 		this.size = 0;
 		this.head = new Entry<E>(null, null, null);
 		this.tail = new Entry<E>(head, null, null);
 		this.head.next = tail;
+		this.map = new HashMap<E, Entry<E>>();
 	}
 	
-	public void Enqueue(E value) {
+	/**
+	 * Add an item to the end of the queue if it is not already present.
+	 * 
+	 * Essentially the enqueue operation of a standard queue, with the
+	 * additional restriction of a set's add operation.
+	 * 
+	 * @param value Item to add to the queue
+	 * @return      True if added, false if already present
+	 */
+	public boolean Enqueue(E value) {
+		if (map.containsKey(value))
+			return false;
+		
 		Entry<E> entry = new Entry<E>(tail.prev, tail, value);
+		this.map.put(value, entry);
 		entry.prev.next = entry;
 		this.tail.prev = entry;
 		this.size++;
+		return true;
 	}
 	
 	public E Dequeue() {
-		Entry<E> first = this.head.next;
-		if (first == tail)
+		if (this.size < 1)
 			throw new IllegalStateException("Cannot Dequeue from an empty queue.");
-		
+
+		Entry<E> first = this.head.next;
+		this.map.remove(first.value);
 		this.head.next = first.next;
 		this.head.next.prev = this.head;
-		
 		this.size--;
 		return first.value;
 	}
 	
 	public E Peek() {
-		Entry<E> first = this.head.next;
-		if (first == tail)
+		if (this.size < 1)
 			throw new IllegalStateException("Cannot Peek an empty queue.");
-		
-		return first.value;
+
+		return this.head.next.value;
 	}
 	
 	public int Size() {
