45b84d5e81689976bc7879af98667d5db3cf6d40
Brandon Mills <mills.brandont@gmail.com>
2013-04-02 03:44:58 -0400

    Java uses E for generic types in collection

--- src/net/bmills/osuqueue/Queue.java
+++ src/net/bmills/osuqueue/Queue.java
@@ -1,6 +1,6 @@
 package net.bmills.osuqueue;
 
-public class Queue<T> {
+public class Queue<E> {
 	
 	private class Entry<T> {
 		// These are going to be public now to simplify things
@@ -15,23 +15,23 @@ public class Queue<T> {
 		}
 	}
 	
-	private Entry<T> head;
-	private Entry<T> tail;
+	private Entry<E> head;
+	private Entry<E> tail;
 	
 	Queue() {
-		this.head = new Entry<T>(null, null, null);
-		this.tail = new Entry<T>(head, null, null);
+		this.head = new Entry<E>(null, null, null);
+		this.tail = new Entry<E>(head, null, null);
 		this.head.next = tail;
 	}
 	
-	public void Enqueue(T value) {
-		Entry<T> entry = new Entry<T>(tail.prev, tail, value);
+	public void Enqueue(E value) {
+		Entry<E> entry = new Entry<E>(tail.prev, tail, value);
 		entry.prev.next = entry;
 		this.tail.prev = entry;
 	}
 	
-	public T Dequeue() {
-		Entry<T> first = this.head.next;
+	public E Dequeue() {
+		Entry<E> first = this.head.next;
 		if (first == tail)
 			throw new IllegalStateException("Cannot Dequeue from an empty queue.");
 		
@@ -41,8 +41,8 @@ public class Queue<T> {
 		return first.value;
 	}
 	
-	public T Peek() {
-		Entry<T> first = this.head.next;
+	public E Peek() {
+		Entry<E> first = this.head.next;
 		if (first == tail)
 			throw new IllegalStateException("Cannot Peek an empty queue.");
 		
